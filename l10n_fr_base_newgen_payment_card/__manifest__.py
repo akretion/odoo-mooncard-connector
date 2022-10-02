e827d2e128d6ae02ae1aba5cd2b2aeec37743dcb
Alexis de Lattre <alexis.delattre@akretion.com>
2022-10-02 23:08:38 +0200

    [FIX] set card_id on transaction when card created on the fly
    Fix module name of l10n_fr_base_newgen_payment_card

--- l10n_fr_base_newgen_payment_card/__manifest__.py
+++ l10n_fr_base_newgen_payment_card/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'New-generation payment card - Base module',
+    'name': 'New-generation payment card - France',
     'version': '14.0.1.0.0',
     'category': 'Accounting',
     'license': 'AGPL-3',
